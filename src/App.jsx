243f595dacaef0bedff458ce4455e25b6181f4be
Mike Sowers <msowers72@yahoo.com>
2023-09-22 16:37:01 -0400

    updated App.jsx added the Pet componet and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-
 import {createRoot} from "react-dom/client";
 import Pet from './Pet';
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Peper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-  ]);
+  return(
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Peper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
