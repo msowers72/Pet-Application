ecb2ea7a7ec2a35d275149de4e2f12c52154b885
Mike Sowers <msowers72@yahoo.com>
2023-09-23 11:26:27 -0400

    created a SearchParmas componet

---
+++ src/SearchParams.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+            type="text"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
