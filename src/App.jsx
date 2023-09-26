a0d5f1c2cf52317daa4d6e91e4aa4dc273a3f43d
Mike Sowers <msowers72@yahoo.com>
2023-09-26 13:54:54 -0400

    set-up React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
-
+import Details from "./Details";
 
 const App = () => {
-  return(
-    <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
-  )
+  return (
+    <BrowserRouter>
+    <h1>Adopt Me!</h1>
+    <Routes>
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<SearchParams />} />
+    </Routes>
+  </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(<App />);
